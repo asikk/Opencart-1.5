51b870858c71c2cf5bc914cb8d58f0792089379b
asikk <asikk@users.noreply.github.com>
2017-02-23 09:39:43 +0200

    add status ORDER_HOLD_APPROVED

--- catalog/controller/payment/wayforpay.php
+++ catalog/controller/payment/wayforpay.php
@@ -188,6 +188,7 @@ class ControllerPaymentWayforpay extends Controller
 class WayForPay
 {
     const ORDER_APPROVED = 'Approved';
+    const ORDER_HOLD_APPROVED = 'WaitingAuthComplete';
 
     const ORDER_SEPARATOR = '#';
 
@@ -310,7 +311,10 @@ class WayForPay
             return 'An error has occurred during payment';
         }
 
-        if ($response['transactionStatus'] == self::ORDER_APPROVED) {
+        if (
+            $response['transactionStatus'] == self::ORDER_APPROVED ||
+            $response['transactionStatus'] == self::ORDER_HOLD_APPROVED
+           ) {
             return true;
         }
 
